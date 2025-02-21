67f275593e4a8b9fb39be5db99a418a6d5b78521
Dagmar87 <jdfssobrinho@hotmail.com>
2025-02-21 11:43:54 -0300

    Atualização das entidades e criação do arquivo NeurotechClientRepository

--- src/main/java/br/com/neurotech/challenge/entity/NeurotechClient.java
+++ src/main/java/br/com/neurotech/challenge/entity/NeurotechClient.java
@@ -1,10 +1,22 @@
 package br.com.neurotech.challenge.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+@Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NeurotechClient {
 	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;	
 	private String name;
 	private Integer age;
 	private Double income;
