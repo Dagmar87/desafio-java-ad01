895a5b10019bd6a74d349e28042ef842a0a21073
Dagmar87 <jdfssobrinho@hotmail.com>
2025-02-21 15:07:06 -0300

    Desenvolvimento das operações no arquivo ClientController

--- src/main/java/br/com/neurotech/challenge/controller/ClientController.java
+++ src/main/java/br/com/neurotech/challenge/controller/ClientController.java
@@ -1,10 +1,19 @@
 package br.com.neurotech.challenge.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.neurotech.challenge.dto.ClientDto;
+import br.com.neurotech.challenge.form.ClientForm;
 import br.com.neurotech.challenge.service.ClientService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/clients")
@@ -12,5 +21,16 @@ public class ClientController {
 	
 	@Autowired
 	private ClientService clientService;
+	
+	@PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+	public ClientDto save(@RequestBody @Valid ClientForm clientForm) {
+		return clientService.save(clientForm);
+	}
+	
+	@GetMapping("/{id}")
+	public ClientDto get(@PathVariable("id") Long id) {
+		return clientService.get(id);
+	}
 
 }
